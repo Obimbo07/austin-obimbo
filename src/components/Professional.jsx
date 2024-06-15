29e63082edf43b33cfecb227c3046a9f6cd05dfd
Obimbo07 <obimboausts@gmail.com>
2024-06-15 15:48:31 +0300

    Feature: 😁 added hover animations

--- src/components/Professional.jsx
+++ src/components/Professional.jsx
@@ -5,15 +5,15 @@ const Professional = () => {
     <div className='flex flex-col md:flex-row justify-center p-4'>
       <div className='md:w-1/3 w-full h-auto mb-4 md:mb-0'>
         <img
-          className='w-full h-auto rounded-md shadow-md'
+          className='w-full h-auto rounded-md shadow-md transform hover:scale-105 transition-transform duration-300'
           src='passport.jpeg'
           alt='professional-passport-photo-austin-obimbo'
         />
       </div>
-      <div className='md:w-2/3 w-full p-4'>
+      <div className='md:w-2/3 w-full p-4 animate-slide-in-left'>
         <div className='mb-4'>
           <h3 className='text-xl font-bold text-blue-600 mb-2'>Introduction</h3>
-          <p className='text-gray-700'>
+          <p className='text-gray-700 hover:text-blue-500 transition-colors duration-300'>
             My name is Austin Obimbo. I am a proactive techprenuer & team leader with 2 years experience as a professional developer.
             I have been able to build and collaborate on high scaled projects with love to prolifiate my growth and impact to the world at large.
             I love travelling, exploring nature, Friendly meetups, Coffee Code sessions, and doing research.
@@ -21,7 +21,7 @@ const Professional = () => {
         </div>
         <div className='mb-4'>
           <h3 className='text-xl font-bold text-blue-600 mb-2'>Skills</h3>
-          <ul className='list-disc list-inside text-gray-700'>
+          <ul className='list-disc list-inside text-gray-700 hover:text-blue-500 transition-colors duration-300'>
             <li><strong>Languages:</strong> HTML/CSS, JavaScript, Ruby, Python</li>
             <li><strong>Frameworks:</strong> ReactJs, Vite, NextJs, ReactNative, Ruby on Rails</li>
             <li><strong>Professional Skills:</strong> Data Analysis, Test Driven Development, Agile Methodology, Team Spirit, Collaboration, Team Leadership, Project Management</li>
@@ -30,8 +30,8 @@ const Professional = () => {
         </div>
         <div className='mb-4'>
           <h3 className='text-xl font-bold text-blue-600 mb-2'>Projects</h3>
-          <p className='text-gray-700'>I have experience with the following environment of projects</p>
-          <ul className='list-disc list-inside text-gray-700'>
+          <p className='text-gray-700 hover:text-blue-500 transition-colors duration-300'>I have experience with the following environment of projects</p>
+          <ul className='list-disc list-inside text-gray-700 hover:text-blue-500 transition-colors duration-300'>
             <li>Content Management System</li>
             <li>Software as a Service</li>
             <li>Financial Management</li>
@@ -41,13 +41,13 @@ const Professional = () => {
         </div>
         <div className='mb-4'>
           <h3 className='text-xl font-bold text-blue-600 mb-2'>Training and Experience</h3>
-          <p className='text-gray-700'>
+          <p className='text-gray-700 hover:text-blue-500 transition-colors duration-300'>
             I have gained most of my skills and knowledge as a self-taught student, and from online bootcamps like Microverse and Coursera. I dedicated 2000+ hours into acquiring knowledge and hands-on experience through remote collaboration and pair-programming with individuals in different time-zones.
           </p>
         </div>
         <div>
           <h3 className='text-xl font-bold text-blue-600 mb-2'>Volunteer</h3>
-          <p className='text-gray-700'>
+          <p className='text-gray-700 hover:text-blue-500 transition-colors duration-300'>
             Qui ex nulla laboris exercitation occaecat. Fugiat quis sunt elit labore. Duis dolor adipisicing tempor in minim cillum occaecat qui.
             Mollit cillum laborum deserunt qui consequat elit. Do do ea dolore cupidatat non. In esse laboris deserunt eiusmod irure culpa ipsum elit.
           </p>
