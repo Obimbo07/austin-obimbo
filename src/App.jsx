a965285af1aaddd6513f1a4bffa6159aae5c2bd7
Obimbo07 <obimboausts@gmail.com>
2024-08-20 23:31:12 +0300

    Features: project assets update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
@@ -11,6 +11,11 @@ import Technologies from './components/Technologies';
 import Contact from './components/Contact';
 
 const App = () => {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer 
+  })
   return (
     <div className="bg-gradient-to-br from-gray-900 to-blue-900">
      <Navbar />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Hero from '../components/Hero';
 import Cards from '../components/Cards';
 import Experience from '../components/Experience';
@@ -12,23 +12,40 @@ import Carousel from '../components/Carousel';
 import AboutText from '../components/AboutText';
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading delay for demonstration
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // Adjust the delay as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <> 
+    <>
       <div className='bg-gradient-to-tr from-blue-900 to-blue-900'>
-        <Carousel />
-        <AboutText />
-         <div className='md:mx-60'>
-           <InteractiveUi />
-         </div>
-        <GithubContibutionGraph />
-        <Experience />
-        <Project />
-        <Partners />
-        <Blog />
-        <Contact />
+        {loading ? (
+          <div className='loader'></div>
+        ) : (
+          <>
+            <Carousel />
+            <AboutText />
+            <div className='md:mx-60'>
+              <InteractiveUi />
+            </div>
+            <GithubContibutionGraph />
+            <Experience />
+            <Project />
+            <Partners />
+            <Blog />
+            <Contact />
+          </>
+        )}
       </div>
     </>
-  )
+  );
 }
 
 export default Home;
