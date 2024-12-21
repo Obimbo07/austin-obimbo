778e1a25ba6b6e0b24fec4cf10be2e1df41e0e6b
Austin Obimbo <obimboausts@gmail.com>
2024-12-21 07:19:39 +0300

    updated exp and achv work data

--- src/components/Data.js
+++ src/components/Data.js
@@ -5,6 +5,8 @@ export const experience = [
     index: 1,
     workTitle: 'Self Employed',
     workLocation: 'Remote',
+    workOrganisation: 'Personal',
+    workLogo: 'logo-shine.png',
     description: 'Worked on 30+ projects as a Full stack developer. Spent 3000+ hours remotely understanding algorithms and data structures using technologies like Ruby on Rails, React (Next.js & Vite), and React Native.',
     date: 'Feb 14th 2023 - Present',
   },
@@ -12,6 +14,26 @@ export const experience = [
     index: 2,
     workTitle: 'Freelance Software Engineer',
     workLocation: 'Remote',
+    workOrganisation: 'Tech To the Rescue',
+    workLogo: 'logo-shine.png',
+    description: 'Helped NGOs and non-profits improve their technological advancements by doing pro-bono and low-bono projects. Worked with technologies like WordPress, CMS, and AI tools to enhance their profiles and output.',
+    date: 'August - September 2024',
+  },
+  {
+    index: 3,
+    workTitle: 'Full stack Developer',
+    workLocation: 'Remote',
+    workOrganisation: 'Digital Tailor Agency',
+    workLogo: 'logo-shine.png',
+    description: '',
+    date: 'September 2024 - Present',
+  },
+  {
+    index: 4,
+    workTitle: 'Freelance Software Engineer',
+    workLocation: 'Remote',
+    workOrganisation: 'Tech To the Rescue',
+    workLogo: 'logo-shine.png',
     description: 'Helped NGOs and non-profits improve their technological advancements by doing pro-bono and low-bono projects. Worked with technologies like WordPress, CMS, and AI tools to enhance their profiles and output.',
     date: 'August 2024 - Present',
   }
@@ -24,6 +46,12 @@ export const achievements = [
     date: '22nd - 24th May 2024',
     description: 'Developed a USSD-Based Surplus Food Distribution System to combat food waste and improve food accessibility using USSD and Africa’s Talking API.',
     link: 'https://www.techtotherescue.org/hackathon/meet-the-winners/',
+  },
+  {
+    index: 0,
+    title: 'Davis and Shirtliff 2024 Hackathon Innovate with AI, IoT and More ',
+    date: '29th July - 29 August 2024',
+    description: 'Won the hackathon in teams of two projects. An Ai product quotation and sizing system and a Technician microservice mobile app. Collaborated remotely and helped contribute to project progress through online meetings. Understood how microservice architecture works and grew knowlegde on developing and training  LLMs for retrival augumented generation(RAG) sytems.',
   },
   
 ];
