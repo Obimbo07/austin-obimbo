7c4e6306a8a22f88127eacabf23a6847652d286e
Austin Obimbo <obimboausts@gmail.com>
2025-01-09 02:59:59 +0300

    fixed achv and exp components design

--- src/components/Achv.jsx
+++ src/components/Achv.jsx
@@ -12,15 +12,15 @@ const Achv = () => {
                 
                 <div className="absolute left-6 top-0 h-full flex flex-col items-center">
                   {i !== achievements.length - 1 && (
-                    <div className="h-full w-1 bg-gradient-to-b from-blue-500 to-purple-500"></div>
+                    <div className="h-full w-1 bg-gradient-to-b from-orange-500 to-orange-900"></div>
                   )}
-                  <div className="h-5 w-5 rounded-full bg-gradient-to-r from-purple-500 to-blue-500 shadow-md"></div>
+                  <div className="h-5 w-5 rounded-full bg-gradient-to-r from-orange-500 to-orange-900 shadow-md"></div>
                 </div>
                 <div className="relative px-16">
 
                   <Card className="shadow-lg bg-gradient-to-r from-white via-gray-50 to-gray-100 hover:shadow-xl hover:scale-105 transition-all duration-300">
                     <CardHeader>
-                      <CardTitle className="text-blue-500 text-2xl font-semibold">{achievement.title}</CardTitle>
+                      <CardTitle className="text-orange-500 text-2xl font-semibold">{achievement.title}</CardTitle>
                       <CardDescription className="text-gray-600">{achievement.date}</CardDescription>
                     </CardHeader>
                     <CardContent>
