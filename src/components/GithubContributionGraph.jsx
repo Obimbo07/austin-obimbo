fd5f2ac607caf7ae3d9a416eb48c033ff319994c
Austin Obimbo <obimboausts@gmail.com>
2025-01-14 15:55:45 +0300

    fixed intro styling and components design

--- src/components/GithubContributionGraph.jsx
+++ src/components/GithubContributionGraph.jsx
@@ -11,24 +11,24 @@ const GitHubContributionGraph = () => {
   };
 
   return (
-    <div className="flex flex-col items-center px-8 my-2 text-white">
+    <div className="flex flex-col items-center px-8 my-2 text-black  font-semibold bg-white rounded-3xl">
      <span className='bg-gray-900 h-2 w-1/2 mb-4 decoration-sky-500/30 underline-offset-8'></span>
      <h1 className='font-extrabold text-4xl text-center w-fit p-2 font-mono antialiased outline'>Github contibution calendar</h1>
-      <div className="mt-10">
+      <div className="my-8">
         <button
-          className="bg-orange-500 text-white p-4 rounded-full mr-2"
+          className="bg-orange-500 text-white p-2 rounded-full mr-2"
           onClick={() => handleYearChange(currentYear)}
         >
           Current Year
         </button>
         <button
-          className="bg-orange-500 text-white p-4 rounded-full"
+          className="bg-orange-500 text-white p-2 rounded-full"
           onClick={() => handleYearChange(currentYear - 1)}
         >
           Previous Year
         </button>
         <button
-          className="bg-orange-500 text-white p-4 rounded-full ml-2"
+          className="bg-orange-500 text-white p-2 rounded-full ml-2"
           onClick={() => handleYearChange('last')}
         >
           Last Full Year
