c152f35a29fdd25ec9d4b1952454d8101e0dc98c
Austin Obimbo <obimboausts@gmail.com>
2025-01-19 14:28:06 +0300

    removed compression and visualizer

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,13 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import path from "path"
 import react from '@vitejs/plugin-react'
-import { visualizer } from 'rollup-plugin-visualizer';
-import compression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
     org: "adata-7a",
     project: "javascript-react"
-  }), visualizer({ open: true }), compression()],
+  })],
 
   resolve: {
     alias: {
@@ -23,20 +21,7 @@ export default defineConfig({
   },
 
   build: {
-    rollupOptions: {
-      output: {
-          manualChunks(id) {
-              if (id.includes('node_modules')) {
-                  return id
-                      .toString()
-                      .split('node_modules/')[1]
-                      .split('/')[0]
-                      .toString();
-                    }
-                  },
-              },
-          },
-          chunkSizeWarningLimit: 1000,
+      chunkSizeWarningLimit: 1000,
       sourcemap: true,
     },
 })
