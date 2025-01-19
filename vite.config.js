fe927294ee420fc3e81077d58cd0a914eebe46b1
Austin Obimbo <obimboausts@gmail.com>
2025-01-19 13:07:25 +0300

    HotFix: Improved website chunks, optimized resources

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,15 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import path from "path"
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer';
+import compression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
     org: "adata-7a",
     project: "javascript-react"
-  })],
+  }), visualizer({ open: true }), compression()],
 
   resolve: {
     alias: {
@@ -21,6 +23,20 @@ export default defineConfig({
   },
 
   build: {
-    sourcemap: true
-  }
+    rollupOptions: {
+      output: {
+          manualChunks(id) {
+              if (id.includes('node_modules')) {
+                  return id
+                      .toString()
+                      .split('node_modules/')[1]
+                      .split('/')[0]
+                      .toString();
+                    }
+                  },
+              },
+          },
+          chunkSizeWarningLimit: 1000,
+      sourcemap: true,
+    },
 })
