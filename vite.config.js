ba9169654325d0080d0d0b1de57d79d2d6e348bc
Austin Obimbo <obimboausts@gmail.com>
2025-01-19 14:21:21 +0300

    HotFix: corrected application optimization

--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,20 @@ export default defineConfig({
   },
 
   build: {
-    sourcemap: true
-  }
+    rollupOptions: {
+      output: {
+          manualChunks(id) {
+              if (id.includes('node_modules')) {
+                  return id
+                      .toString()
+                      .split('node_modules/')[1]
+                      .split('/')[0]
+                      .toString();
+                    }
+                  },
+              },
+          },
+          chunkSizeWarningLimit: 1000,
+      sourcemap: true,
+    },
 })
