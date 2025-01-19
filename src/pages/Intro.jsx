97f5d8eb7bc629d77caaf4ccf02151d0bdddd53b
Austin Obimbo <obimboausts@gmail.com>
2025-01-19 03:57:07 +0300

    removed color bg in intro

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -31,7 +31,7 @@ import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
 
 const Intro = () => {
   return (
-    <div className="bg-[#f8f2f0] bg-gray-400 min-h-screen p-4">
+    <div className=" min-h-screen p-4">
       <TawkMessengerReact
                 propertyId="64ce31b8cc26a871b02d6f71"
                 widgetId="1ihl1mn2g"
