4c61a228457fef3e068a0e025f8c71c24bfc55d9
Austin Obimbo <obimboausts@gmail.com>
2025-02-02 18:19:18 +0300

    updated intro component design

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -31,7 +31,7 @@ import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
 
 const Intro = () => {
   return (
-    <div className="min-h-screen p-4">
+    <div className="min-h-screen pt-2">
       <TawkMessengerReact
                 propertyId="64ce31b8cc26a871b02d6f71"
                 widgetId="1ihl1mn2g"
@@ -42,20 +42,20 @@ const Intro = () => {
         <div className="flex flex-col gap-2">
             <div className="flex gap-2">
                 <div className="flex flex-none flex-col w-2/3 justify-center items-start gap-4 p-4 rounded-3xl bg-white shadow-lg">
-                    <h2 className="text-2xl md:text-4xl font-bold leading-tight">
+                    <h2 className="text-2xl md:text-4xl font-serif font-bold leading-tight">
                         <span className="">Design</span><br />
                         <span className="">Development</span><br />
                         <span className="text-orange-500">Architecture</span>
                     </h2>
                     <p className="text-gray-600">
-                        Austin Obimbo is an innovative software developer who creates
+                        Austin Obimbo is an innovative software developer | AI Research Scientist who creates
                         architecture redefining modern living spaces.
                     </p>
                     <div className="flex justify-between w-full ">
-                        <Link href='https://docs.google.com/document/d/16JNuFYkcSz5_eDSnlnLinkezV_bU9NClhMa/edit?usp=sharing&ouid=107251804543803193655&rtpof=true&sd=true' target="_blank" className="bg-orange-800 hover:bg-gray-100 hover:text-black rounded-full text-white p-2">
-                         Get Resume
+                        <Link href='https://docs.google.com/document/d/16JNuFYkcSz5_eDSnlnLinkezV_bU9NClhMa/edit?usp=sharing&ouid=107251804543803193655&rtpof=true&sd=true' target="_blank" className="bg-gray-800 hover:bg-gray-100 hover:text-black rounded-full text-white p-2">
+                         Resume
                        </Link>
-                       <Link to="/contact" className="bg-orange-800 hover:bg-gray-100 hover:text-black rounded-full text-white p-2">
+                       <Link to="/contact" className="bg-gray-800 hover:bg-gray-100 hover:text-black rounded-full text-white p-2">
                         Contact me
                        </Link>
                     </div>
@@ -66,7 +66,7 @@ const Intro = () => {
             </div>
             <div className="flex gap-2">
                 <div className=" w-2/3 flex text-center flex-none flex-col size-full justify-center items-start gap-4 rounded-3xl bg-white shadow-lg">
-                  <h1 className="text-3xl w-full font-extrabold">Services</h1>
+                  <h1 className="text-xl w-full font-sans font-bold">WHAT I DO BEST</h1>
                   <Services />
                 </div>
                 <div className="flex flex-grow toolbox hover:opacity-100 opacity-70 flex-col justify-center items-start rounded-3xl shadow-lg outline outline-1 w-1/3 outline-offset-2 outline-neutral-100">
@@ -81,16 +81,16 @@ const Intro = () => {
           
           <div className="rounded-3xl shadow-lg bg-white h-auto flex flex-col">
               <div className="flex justify-between p-4">
-                 <h1 className="text-3xl md:text-4xl font-bold leading-tight">Austin Obimbo</h1>
-                 <a href='https://docs.google.com/document/d/16JNuFYkcSz5_eDSnlnAezV_bU9NClhMa/edit?usp=sharing&ouid=107251804543803193655&rtpof=true&sd=true' target="_blank"  className="bg-orange-800 font-bold p-2 rounded-full text-white">
-                   Get Resume
+                 <h1 className="text-2xl md:text-4xl font-bold leading-tight font-serif">Austin Obimbo</h1>
+                 <a href='https://docs.google.com/document/d/16JNuFYkcSz5_eDSnlnAezV_bU9NClhMa/edit?usp=sharing&ouid=107251804543803193655&rtpof=true&sd=true' target="_blank"  className="bg-green-800 font-bold p-2 rounded-full text-white">
+                   Resume
                   <FontAwesomeIcon icon={faArrowRightToFile} />
                  </a>
               </div>
              <div className="w-full">
                 <div className="booking-image p-12 mx-8 rounded-3xl">
-                <div className="community-wall bg-orange-800 shadow-2xl text-white relative opacity-90 text-sm hover:opacity-90 rounded-3xl grow">
-                  <p className="font-sans text-white text-center text-3xl font-bold p-2">Community wall</p>
+                <div className="community-wall bg-gray-800 shadow-2xl text-white relative opacity-90 text-sm hover:opacity-90 rounded-3xl grow">
+                  <p className="text-white text-center text-xl font-bold p-2">Community wall</p>
                   <h2 className="hidden font-sans text-white text-center font-bold p-2 text-2xl bg-dark-900">Coming Soon</h2>
                 </div>
                 </div>
@@ -100,8 +100,8 @@ const Intro = () => {
                 <AccordionItem value="item-1">
                   <AccordionTrigger className="">
                     <p 
-                      className="font-extrabold text-orange-800 text-center text-3xl">
-                      Work Experience
+                      className="font-bold text-center text-3xl">
+                      WORK EXPERIENCE
                       </p>
                   </AccordionTrigger>
                   <AccordionContent className="">
@@ -113,8 +113,8 @@ const Intro = () => {
                 <AccordionItem value="item-2">
                   <AccordionTrigger className="">
                     <p 
-                      className="font-extrabold text-orange-800 text-center text-3xl">
-                      Achievements
+                      className="font-bold text-center text-3xl">
+                      ACHIEVEMENTS
                       </p>
                   </AccordionTrigger>
                   <AccordionContent>
